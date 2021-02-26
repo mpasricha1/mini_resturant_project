8a73779793c8d3ba41e97a4de027a9e0eabb7ae6
Jacob Hohing <jakehohing@yahoo.com>
2021-02-25 20:32:54 -0500

    Routes added

--- routes/routes.js
+++ routes/routes.js
@@ -3,6 +3,32 @@ const fileUtil = require("../utils/fileUtils")
 
 module.exports = app =>{
 	app.get('/', (req, res) =>{
-		res.sendFile(path.join(__dirname, "../public/index.html"))
+		res.sendFile(path.join(__dirname, "../public/home.html"))
 	});
+
+	app.get('/', (req, res) =>{
+		res.sendFile(path.join(__dirname, "../public/tables.html"))
+	});
+
+	app.get('/', (req, res) =>{
+		res.sendFile(path.join(__dirname, "../public/make.html"))
+	});
+
+	app.get('/', (req, res) =>{
+		res.sendFile(path.join(__dirname, "../api/tables.json"))
+	});
+
+	app.get('/', (req, res) =>{
+		res.sendFile(path.join(__dirname, "../api/waitlist.json"))
+	});
+
+	app.get("/api/tables", function(req,) {
+		return fileUtils.getData("tables")
+	});
+	
+	app.get("/api/tables", function(req,) {
+		return fileUtils.getData("waitlist")
+	});
+
+
 }
