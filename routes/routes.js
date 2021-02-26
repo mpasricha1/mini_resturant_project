e96727ca7d61defb937c0e5cd2d689694e028c1d
Jacob Hohing <jakehohing@yahoo.com>
2021-02-25 21:06:09 -0500

    Added AJAX call

--- routes/routes.js
+++ routes/routes.js
@@ -1,29 +1,21 @@
 const path = require("path")
-const fileUtil = require("../utils/fileUtils")
+const fileUtil = require("../utils/fileutils")
 
 module.exports = app =>{
 	app.get('/', (req, res) =>{
-		res.sendFile(path.join(__dirname, "../public/"))
+		res.sendFile(path.join(__dirname, "../public/home.html"))
 	});
 	
-	app.get('/', (req, res) =>{
+	app.get('/home', (req, res) =>{
 		res.sendFile(path.join(__dirname, "../public/home.html"))
 	});
 
-	app.get('/', (req, res) =>{
+	app.get('/tables', (req, res) =>{
 		res.sendFile(path.join(__dirname, "../public/tables.html"))
 	});
 
-	app.get('/', (req, res) =>{
+	app.get('/make', (req, res) =>{
 		res.sendFile(path.join(__dirname, "../public/make.html"))
-	});
-
-	app.get('/', (req, res) =>{
-		res.sendFile(path.join(__dirname, "../api/tables.json"))
-	});
-
-	app.get('/', (req, res) =>{
-		res.sendFile(path.join(__dirname, "../api/waitlist.json"))
 	});
 
 	app.get("/api/tables", function(req, res) {
